c2134f55fe81e6dce6b738fd4a39d255fda3141d
Jörg Vollmer <javacook@gmx.de>
2019-02-15 17:23:37 +0100

    Kleine Performance-Verbesserung (step(2))

--- src/main/kotlin/Goldbach.kt
+++ src/main/kotlin/Goldbach.kt
@@ -17,7 +17,7 @@ fun Int.isPrime(): Boolean {
         this in 0..1 -> false
         this == 2 -> true
         2 divides this -> false
-        else -> (3..this).any { it*it <= this && it divides this }.not()
+        else -> (3..this).step(2).any { it*it <= this && it divides this }.not()
     }
 }
 
